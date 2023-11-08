b7a59217e4749d9dd61d953dc138f4e32193dcb6
evgen <evgen@DESKTOP-KV4AHG5>
2023-11-08 14:47:46 +0300

    палиндром

---
+++ recursionHomeWork/Palin.cs
@@ -0,0 +1,23 @@
+﻿var list = new List<char>();
+var str = Console.ReadLine();
+var strSecond = str.ToLower();
+foreach (var item in strSecond)
+{
+    if (item != ' ')
+    {
+        list.Add(item);
+    }
+}
+static string GetPalin(List<char> list, int first, int last)
+{
+    if (first >= last)
+    {
+        return "Палиндром";
+    }
+    if (list[first] == list[last])
+    {
+        return GetPalin(list, first + 1, last - 1);
+    }
+    return "Не палиндром";
+}
+Console.WriteLine(GetPalin(list, 0, list.Count - 1));
---
+++ recursionHomeWork/SymArray.cs
@@ -0,0 +1,10 @@
+﻿//var array = new int[] {1,2,3,4,3,2,1};
+//static string GetSymArray(int[] array,int first,int last)
+//{
+//    if (first >= last)
+//        return "Симметричен";
+//    if (array[first] == array[last])
+//        return GetSymArray(array,first + 1,last - 1);
+//    return "Не симметрия";
+//}
+//Console.WriteLine(GetSymArray(array,0,array.Length - 1));
