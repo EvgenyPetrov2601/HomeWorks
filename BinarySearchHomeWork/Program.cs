abed90847225e98568a4ea48d44701859f8b55be
evgen <evgen@DESKTOP-KV4AHG5>
2023-11-09 07:33:38 +0300

    BinarySearch

---
+++ BinarySearchHomeWork/Program.cs
@@ -0,0 +1,26 @@
+﻿
+static int BinarySearch(int[] array, int searchedValue, int left, int right)
+{
+
+    while (left <= right)
+    {
+        var middle = (left + right) / 2;
+
+        if (searchedValue == array[middle])
+        {
+            return middle;
+        }
+        else if (searchedValue < array[middle])
+        {
+            right = middle - 1;
+        }
+        else
+        {
+            left = middle + 1;
+        }
+    }
+    return -1;
+}
+var array = new int[] { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
+var n = int.Parse(Console.ReadLine());
+Console.WriteLine($"index = {BinarySearch(array, n, 0, array.Length)}");
